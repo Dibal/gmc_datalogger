0364ec02376b4242f5be592db29ef81c8913f59f
Dominik <meyerd@mytum.de>
2018-01-26 17:03:40 +0100

    whitespace changes

--- gmcparse6.py
+++ gmcparse6.py
@@ -12,15 +12,16 @@
 import datetime
 import struct
 import time
+import sys
 import serial       
 
 
-vers="GMCparse 2017-03-17"
-device="GMC-320"
-#verbose=1
-debug=0
-limitlines=80  # limit output lines to screen if not verbose
-fullout=True
+vers = "GMCparse 2017-03-17"
+device = "GMC-320"
+#verbose = 1
+debug = 0
+limitlines = 80  # limit output lines to screen if not verbose
+fullout = True
 
 
 def getVER(ser):
@@ -39,9 +40,9 @@ def getCPM(ser):
     ser.write(b'<GETCPM>>')
     rec = ser.read(2)
     try:
-        s1= ord(rec[0])<< 8 | ord(rec[1])
+        s1 = ord(rec[0]) << 8 | ord(rec[1])
     except IndexError:
-        s1= ord("\x00")
+        s1 = ord("\x00")
     return s1
     
     
@@ -50,11 +51,11 @@ def getDate(ser):
     """
     rec = serialCOMM(ser, b'<GETDATETIME>>', 7, False) 
     dsb = [ord(i) for i in rec[:6]]
-    dsb[0] +=2000
+    dsb[0] += 2000
     return datetime.datetime(*dsb).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def getSPIR(ser, address = 0, datalength = 4096):
+def getSPIR(ser, address=0, datalength=4096):
     # by ullix
     # Request history data from internal flash memory
     # Command:  <SPIR[A2][A1][A0][L1][L0]>>
@@ -87,12 +88,14 @@ def getSPIR(ser, address = 0, datalength = 4096):
     # pack address into 4 bytes, big endian; then clip 1st byte = high byte!   
     ad = struct.pack(">I", address)[1:] 
     if debug:
-        print "SPIR address:\t\t\t{:5d}, hex chars in SPIR command: {:02x} {:02x} {:02x}".format(address, ord(ad[0]), ord(ad[1]), ord(ad[2]))
+        print "SPIR address:\t\t\t{:5d}, hex chars in SPIR command: {:02x} {:02x} {:02x}".format(address, ord(ad[0]),
+                                                                                                 ord(ad[1]), ord(ad[2]))
 
     # pack datalength into 2 bytes, big endian; use all bytes
     dl = struct.pack(">H", datalength-1)  
     if debug:
-        print "SPIR datalength requested:\t{:5d}, hex chars in SPIR command: {:02x} {:02x}".format(datalength, ord(dl[0]), ord(dl[1]))
+        print "SPIR datalength requested:\t{:5d}, hex chars in SPIR command: {:02x} {:02x}".format(datalength,
+                                                                                                   ord(dl[0]), ord(dl[1]))
 
     # returns string of characters with each chr-value from 0...255
     # NOT converted into list of int
@@ -102,7 +105,7 @@ def getSPIR(ser, address = 0, datalength = 4096):
     return rec
 
     
-def serialCOMM(ser, sendtxt, returnlength, byteformat = True):
+def serialCOMM(ser, sendtxt, returnlength, byteformat=True):
     # write to and read from serial port
     # exit on comm error
     # if byteformat is True, then convert received string to list of int
@@ -129,15 +132,15 @@ def readHIST(ser):
     """
     # (ullix and mod)
     
-    verbose=""
-    allspir =""
+    verbose = ""
+    allspir = ""
     # read data from device
     for chunk in range(0, 16): # all 64k data
     #for chunk in range(0, 1):   # only first 4096 bytes
         time.sleep(0.5)         # fails occasionally to read all data
                                 # when sleep is too short
         allspir += getSPIR(ser, chunk * 4096 , 4096)
-        if (ord(allspir[-1])== 0xff) and  (ord(allspir[-2]) == 0xff) and (ord(allspir[-3])== 0xff):
+        if (ord(allspir[-1]) == 0xff) and (ord(allspir[-2]) == 0xff) and (ord(allspir[-3]) == 0xff):
             if verbose:
                 print "Device memory: {:02d} pages (4096 each) out of 16 read".format(chunk+1)
             break
@@ -174,39 +177,38 @@ def analyse(form, data, fn, verbose=True):
     55 AA 02 str_length chr1 chr2 ...
     """
 
-    idcpm=2 # id_cpm 
-    tick= datetime.timedelta(minutes=1)
-    dp=0    # data pointer
-    ccount=0  # counted events; <0 if no counter value found but date or other control value
-    msgv='' #verbose message
-    msge='' #error message
-    fullout=False
-    limitlines=30
+    idcpm = 2 # id_cpm
+    tick = datetime.timedelta(minutes=1)
+    dp = 0    # data pointer
+    ccount = 0  # counted events; <0 if no counter value found but date or other control value
+    msgv = '' #verbose message
+    msge = '' #error message
+    fullout = False
+    limitlines = 30
 
-    datenow= datetime.datetime(1950,01,22, 11,12, 13)  #  arabitrary time as init value (< 2000)
+    datenow = datetime.datetime(1950, 01, 22, 11, 12, 13)  #  arabitrary time as init value (< 2000)
     
     if fn:
-        f=open(fn,"w")
+        f = open(fn, "w")
 
-    while dp<len(data):
-        
+    while dp < len(data):
         # test whether data is counter value
-        ccount=ord(data[dp])
-        if (ord(data[dp])==0x55) and (ord(data[dp+1])==0xaa):
-            if ord(data[dp+2])==0x00:
+        ccount = ord(data[dp])
+        if (ord(data[dp]) == 0x55) and (ord(data[dp+1]) == 0xaa):
+            if ord(data[dp+2]) == 0x00:
                     
                 # date + time found (very likely)
-                if (ord(data[dp+9])==0x55) and (ord(data[dp+10])==0xaa):
-                    if ord(data[dp+11])<4:
+                if (ord(data[dp+9]) == 0x55) and (ord(data[dp+10]) == 0xaa):
+                    if ord(data[dp+11]) < 4:
                         if ord(data[dp+11]) != idcpm:
-                            idcpm= ord(data[dp+11]) 
+                            idcpm = ord(data[dp+11])
                             if verbose:
-                                msgv= "* Time intv. mod. (0: off, 1: sec, 2: min, 3: hour): {:0d}".format(idcpm)
+                                msgv = "* Time intv. mod. (0: off, 1: sec, 2: min, 3: hour): {:0d}".format(idcpm)
                             # define time interval
                             if idcpm == 2:
-                                tick= datetime.timedelta(minutes=1)
+                                tick = datetime.timedelta(minutes=1)
                             elif idcpm == 1:
-                                tick=  datetime.timedelta(seconds=1)
+                                tick = datetime.timedelta(seconds=1)
                             elif idcpm == 3:
                                 tick = datetime.timedelta(minutes=60)
                             elif idcpm == 0:
@@ -216,16 +218,16 @@ def analyse(form, data, fn, verbose=True):
                             else: 
                                 # should not happen
                                 if verbose:
-                                    print "Undefined history interval; should be 0,1,2,3; found: {:2d}".format( ord(data[dp+11]) )
-                                    print "{:0X}{:0X}{:0X}".format(ord(data[dp+9]), ord(data[dp+10]), ord(data[dp+11]) )
+                                    print "Undefined history interval; should be 0,1,2,3; found: {:2d}".format(ord(data[dp+11]))
+                                    print "{:0X}{:0X}{:0X}".format(ord(data[dp+9]), ord(data[dp+10]), ord(data[dp+11]))
                     #print "*", ord(data[dp+11])
                     ds = data[dp+3:dp+9]
                     dsb = [ord(i) for i in ds]
-                    dsb[0] +=2000
-                    datenow=datetime.datetime(*dsb)
+                    dsb[0] += 2000
+                    datenow = datetime.datetime(*dsb)
                     if verbose:
-                        msgv= "* {:%Y-%m-%d %H:%M:%S} Dev. Timetag;  {:d}".format(datenow, idcpm)
-                    ccount=-1
+                        msgv = "* {:%Y-%m-%d %H:%M:%S} Dev. Timetag;  {:d}".format(datenow, idcpm)
+                    ccount = -1
                     # next value: 11 + 1=12; additional single incremented below
                     dp += 11
                 else:
@@ -234,43 +236,43 @@ def analyse(form, data, fn, verbose=True):
                         print "No date/time endmarker found; treat markers as counting data"
                     
             # double byte found
-            elif ord(data[dp+2])==1:
+            elif ord(data[dp+2]) == 1:
                 # double byte found
-                ccount = struct.unpack('>H',data[dp+3]+data[dp+4])[0]
+                ccount = struct.unpack('>H', data[dp+3]+data[dp+4])[0]
                 #print "***", ord(data[dp+2]), ord(data[dp+3]), ord(data[dp+4]), ord(data[dp+5]), getnumber, '***'
                 #print ccount, "****",
                 #next value: 4+1=5; additional single incremented below
-                dp +=4
+                dp += 4
                         
             # string found
-            elif ord(data[dp+2])==2:
+            elif ord(data[dp+2]) == 2:
                 # ascii string found
-                strgl=ord(data[dp+3])
-                strg=data[dp+4:dp+4+strgl]
+                strgl = ord(data[dp+3])
+                strg = data[dp+4:dp+4+strgl]
                 if verbose:
-                    msgv= "* ID tag: {:s}".format(strg)
-                ccount=-1
+                    msgv = "* ID tag: {:s}".format(strg)
+                ccount = -1
                 #additional single incremented below
                 dp = dp+3+strgl
 
         #print " {:02X}".format(ord(data[dp])),   
         
         ##prepare output
-        if ccount>= 0:
+        if ccount >= 0:
             #print "{:02X}".format(ccount),
 
             #print datenow , ccount
-            sp="{:%Y-%m-%d %H:%M:%S} {:5d}".format(datenow, ccount)
+            sp = "{:%Y-%m-%d %H:%M:%S} {:5d}".format(datenow, ccount)
             
             # print only a few lines to screen
             #if fullout or (dp<limitlines):
 
             # print only a few lines to screen
-            if fullout or (dp<limitlines):
+            if fullout or (dp < limitlines):
                 print sp
             form.writeplain(sp)
             if fn:
-                f.write(sp+ "\n")
+                f.write(sp + "\n")
             datenow += tick
         else:
             if verbose:
@@ -278,16 +280,15 @@ def analyse(form, data, fn, verbose=True):
                 print msgv
                 form.writeplain(msgv)
                 if fn:
-                    f.write(msgv+"\n")     
+                    f.write(msgv + "\n")
         dp += 1
-        if dp>65536:
-            msge= "Invalid data file (> 16 * 4096)"
+        if dp > 65536:
+            msge = "Invalid data file (> 16 * 4096)"
             exit(1)
-    if datenow.year<2000:
+    if datenow.year < 2000:
         msge = "No date/time tag found. Valid data?"
     if fn:
         f.close()
     form.writeplain("*** done ***")
     print msge
-    return(msge)
-        
+    return (msge)
--- gwcmain.py
+++ gwcmain.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-s
 
 
-'''
+"""
 Read and process history data recorded by Geiger Counter GMC-3xx
 
 Vers 0.7, 2017-03-17 (rimbus)
 
 Software may be free to use according to GPL3
 https://www.gnu.org/licenses/gpl-3.0.de.html 
-'''
+"""
 
 
 from PyQt4 import QtGui  
@@ -21,7 +21,6 @@ import time
 
 
 class GmcApp(QtGui.QMainWindow, gwcp3.Ui_MainWindow):
-    
     def __init__(self):
         global data
         global ser
@@ -33,13 +32,13 @@ class GmcApp(QtGui.QMainWindow, gwcp3.Ui_MainWindow):
         self.setupUi(self)  # automatically created in gwcp
         
         # define some global variables (might be improved later)
-        helptextname="help.md"
-        data =''
-        ser=''
-        serialdev='/dev/ttyUSB0'
-        #speed= 115200
-        speedid=0
-        #ser=serial.Serial('/dev/ttyUSB0', 115200, timeout= 3)  
+        helptextname = "help.md"
+        data = ''
+        ser = ''
+        serialdev = '/dev/ttyUSB0'
+        #speed = 115200
+        speedid = 0
+        #ser = serial.Serial('/dev/ttyUSB0', 115200, timeout= 3)
         
         self.statusBar().showMessage("...") 
         
@@ -49,33 +48,33 @@ class GmcApp(QtGui.QMainWindow, gwcp3.Ui_MainWindow):
         
         # preselect baud rate
         self.listWidgetspeed.setCurrentRow(speedid)
-        speed=int(self.listWidgetspeed.currentItem().text())
+        speed = int(self.listWidgetspeed.currentItem().text())
         self.lineEditsetDev.setText(serialdev)
         
-        #load bin file
+        # load bin file
         self.pushButtonLoadBin.clicked.connect(self.readbinfile)
         
-        #load data from device
+        # load data from device
         self.pushButtonLoadDevice.clicked.connect(self.readdev)
         
-        #check serial
+        # check serial
         self.pushButtonSerialSet.clicked.connect(self.checkserial)
         
-        #process data
+        # process data
         self.pushButton.clicked.connect(self.processdata)
         
-        #write data
+        # write data
         self.pushButtonwritedata.clicked.connect(self.writedatafile)
         
-        #time info
+        # time info
         self.pushButtongetTime.clicked.connect(self.timeinfo)
         
-        #live data
+        # live data
         self.pushButtonLiveData.setCheckable(True)
-        #.pushButtonLiveData.setChecked(False)
+        # self.pushButtonLiveData.setChecked(False)
         self.pushButtonLiveData.clicked.connect(self.livedata)
         
-    #menubar
+    # menubar
     def windowaction(self, q):
         global helptextname
         global ser
@@ -83,102 +82,96 @@ class GmcApp(QtGui.QMainWindow, gwcp3.Ui_MainWindow):
         print "triggered by menubar", q.text()
         if q.text() == "&Quit":
             sys.exit()
-        elif q.text() =="Show Helpfile":
+        elif q.text() == "Show Helpfile":
             with open(helptextname, mode='rb') as file:  
                 helptext = file.read()  
             self.clearplain()
             self.writeplain(helptext)
 
-        
     def readbinfile(self):
         global data
         
         fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '',"Data files (*.*)")
-        with open(fname, mode='rb') as file:  
-            data = file.read() 
-        msg= "{:d} bytes loaded from {:s}".format(len(data),fname)
+        with open(fname, mode='rb') as f:
+            data = f.read()
+        msg = "{:d} bytes loaded from {:s}".format(len(data), fname)
         self.statusBar().showMessage(msg) 
         self.processdata()
-    
-    
+
     def readdev(self):
         global ser, data
         
         # ser not defined in init in order to process files even if no device connected
         if not ser:
-            #print "set ser"
-            #ser = serial.Serial('/dev/ttyUSB0', 115200, timeout= 3) 
-            ser = serial.Serial(serialdev, speed, timeout= 3) 
+            # print "set ser"
+            # ser = serial.Serial('/dev/ttyUSB0', 115200, timeout= 3)
+            ser = serial.Serial(serialdev, speed, timeout=3)
         self.statusBar().clear()
         self.statusBar().showMessage("loading...") 
         print "loading..."
         data = readHIST(ser)
-        msg= "{:d} bytes read from device".format(len(data))
+        msg = "{:d} bytes read from device".format(len(data))
         self.statusBar().showMessage(msg) 
         self.processdata()
 
-        
     def checkserial(self):
         global speed
         global serialdev
         global ser
         
-        serialdev= self.lineEditsetDev.text().toUtf8()
-        #speed= int(self.listWidgetspeed.item(1).text())
-        speed=int(self.listWidgetspeed.currentItem().text())
-        msg="Serial interface: {:s} at {:d} baud".format(serialdev, speed)
-        ser = serial.Serial(serialdev, speed, timeout= 3) 
+        serialdev = self.lineEditsetDev.text().toUtf8()
+        # speed= int(self.listWidgetspeed.item(1).text())
+        speed = int(self.listWidgetspeed.currentItem().text())
+        msg = "Serial interface: {:s} at {:d} baud".format(serialdev, speed)
+        ser = serial.Serial(serialdev, speed, timeout=3)
         self.statusBar().showMessage(msg)
-        
 
     def writedatafile(self):
         global data
         
-        if len(data)>1:
-            fn= QtGui.QFileDialog.getSaveFileName(self, "Save file", "", "*.*")
+        if len(data) > 1:
+            fn = QtGui.QFileDialog.getSaveFileName(self, "Save file", "", "*.*")
             if self.radioButtonBin.isChecked():
                 print "binary choosen"
-                with open(fn, mode='wb') as file:  
-                    file.write(data)  
-                msgs= "{:d} bytes saved to {:s}".format(len(data), fn)
+                with open(fn, mode='wb') as f:
+                    f.write(data)
+                msgs = "{:d} bytes saved to {:s}".format(len(data), fn)
             else:
                 analyse(self, data, fn, self.checkBox.isChecked())
-                msgs= "{:d} bytes converted and saved to {:s}".format(len(data), fn)
+                msgs = "{:d} bytes converted and saved to {:s}".format(len(data), fn)
         else:
-            msgs= "No data to save"
+            msgs = "No data to save"
         self.statusBar().showMessage(msgs)
-            
 
-    #process data
+    # process data
     def processdata(self):
         self.plainTextEdit.clear()
-        msgs= analyse(self, data, '', self.checkBox.isChecked())
+        msgs = analyse(self, data, '', self.checkBox.isChecked())
         if msgs:
             self.plainTextEdit.appendPlainText(msgs)
             self.statusBar().showMessage("Valid data?")
         else:
             pass
-            #self.statusBar().showMessage("done")
-
+            # self.statusBar().showMessage("done")
 
     def timeinfo(self):
         global ser
         
         if not ser:
-            ser = serial.Serial(serialdev, speed, timeout= 3) 
-        dtime=getDate(ser)
-        stime= "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())
+            ser = serial.Serial(serialdev, speed, timeout=3)
+        dtime = getDate(ser)
+        stime = "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())
         print "GMC device date:", dtime
         print "(interface date: ", stime
         self.lineEditgetTime.setText(dtime)
         self.lineEditSystemTime.setText(stime)
         print "enter demo  loop"
-        for i  in range(0,1000000):
+        for i in range(0, 1000000):
             i*i*i
             QtGui.QApplication.processEvents()
         print "demoloop ended"
             
-    #requires pushButtonLiveData
+    # requires pushButtonLiveData
     def livedata(self):
         global ser
         
@@ -189,17 +182,16 @@ class GmcApp(QtGui.QMainWindow, gwcp3.Ui_MainWindow):
             self.pushButtonLiveData.setStyleSheet("background-color: red")
         else:
             self.pushButtonLiveData.setStyleSheet("background-color: none")
-        ta=''
+        ta = ''
         while self.pushButtonLiveData.isChecked():
             QtGui.QApplication.processEvents()
             ds= "{:s}  {:d}".format(getDate(ser), getCPM(ser))
             tn= ds[-6:-4]
             if ta != tn:
                 self.writeplain(ds)
-                ta=tn
-            #print "Checked? ", self.pushButtonLiveData.isChecked()
+                ta = tn
+            # print "Checked? ", self.pushButtonLiveData.isChecked()
 
-    
     def writeplain(self, ln):
         self.plainTextEdit.appendPlainText(ln)
         
